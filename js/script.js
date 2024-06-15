953f4b0e7d9c98528930f0574a7ea0ddf0e075bc
Blacker <parsafarivash5713@gmail.com~>
2024-06-15 15:36:43 +0330

    AAD project to git

---
+++ js/script.js
@@ -0,0 +1,94 @@
+let $ = document
+
+let userNameInput = $.querySelector('.username')
+let passWordInput = $.querySelector('.password')
+let modal = $.querySelector('.modal')
+
+
+function dataValidation() {
+    let userNameValue = userNameInput.value
+    let passwordValue = passWordInput.value
+
+    if (userNameValue.length < 12 || passwordValue.length < 8) {
+        // alert('Error')
+        modal.style.background = 'rgb(223, 28, 28)'
+        modal.innerHTML = 'لطفا اطلاعات را به درستی وارد نمایید'
+        modal.style.display = 'inline'
+
+    } else {
+        modal.style.background = 'green'
+        modal.innerHTML = 'لاگین با موفقیت انجام شد'
+        modal.style.display = 'inline'
+    }
+
+    setTimeout(function () {
+        modal.style.display = 'none'
+    }, 3000)
+}
+
+// let usernameMessage = document.querySelector('.username-validation')
+// let passwordMessage = document.querySelector('.password-validation')
+
+// let usernameInput = document.querySelector('.username')
+// let passwordInput = document.querySelector('.password')
+
+// function usernameValidathion() {
+//     //alert('pidaRET')
+//     if (usernameInput.value.length < 8) {
+//         usernameMessage.innerHTML = 'At least 12 characters'
+//         usernameMessage.style.display = 'block'
+//     } else {
+//         usernameMessage.style.display = 'none'
+//     }
+// }
+// function passwordValidation() {
+//     //alert('PIODAREYWE')
+//     if (passWordInputInput.value.length < 8) {
+//         passwordMessage.innerHTML = 'At least 8 characters'
+//         passwordMessage.style.display = 'block'
+//     } else {
+//         passwordMessage.style.display = 'none'
+//     }
+// }
+
+let usernameInput = $.querySelector('.username')
+let passwordInput = $.querySelector('.password')
+
+let usernameMessage = $.querySelector('.username-validation')
+let passwordMessage = $.querySelector('.password-validation')
+
+
+function usernameValidation() {
+    if (usernameInput.value.length < 12) {
+        usernameMessage.style.display = 'block'
+        userNameInput.style["boxShadow"] = "0px 0px 20px 0px red"
+        usernameMessage.style["padding-bottom"] = "13px"
+
+    } else {
+        usernameMessage.style.display = 'none'
+        userNameInput.style["boxShadow"] = "0px 0px 0px 0px red"
+        passwordMessage.style["padding-bottom"] = "13px"
+    }
+}
+function passwordValidation() {
+    if (passwordInput.value.length < 8) {
+        passwordMessage.style.display = 'block'
+        passWordInput.style["boxShadow"] = "0px 0px 20px 0px red"
+    }else {
+        passwordMessage.style.display = 'none'
+        passWordInput.style["boxShadow"] = "0px 0px 0px 0px red"
+
+    }
+}
+
+let redValue, greenValue, BlueValue
+let lomp = $.querySelector('.lomp')
+
+setInterval(function () {
+
+    redValue = Math.floor(Math.random() * 255)
+    greenValue = Math.floor(Math.random() * 255)
+    BlueValue = Math.floor(Math.random() * 255)
+
+    lomp.style.backgroundColor = 'rgb(' + redValue + ',' + greenValue + ',' + BlueValue + ')'
+}, 1000)
