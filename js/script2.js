06cc76686c5bba36950e2b4a0919ef2133328673
Blacker <parsafarivash5713@gmail.com~>
2024-06-21 13:58:51 +0330

    added the menu code and the preparation of weather codes

--- js/script2.js
+++ js/script2.js
@@ -1,1 +1,28 @@
-﻿//  (: همین الان شروع کنین و بنویسین
+﻿let countrySelectBox = document.querySelector('.countrySelect')
+let citySelect = document.querySelector('.citySelect')
+
+let countriesData = {
+    Iran: ['Tehran', 'Shiraz', 'Tabriz','Esfehan', 'Arak', 'Pasargadae'],
+    Turkey: ['Error ***'],
+    US:['Los Angles', 'San Diego', 'New York']
+}
+
+countrySelectBox.addEventListener('change', function () {
+
+    if (countrySelectBox.value === 'Please Select') {
+        citySelect.innerHTML = '';
+        citySelect.innerHTML += "<option>Select City ...</option>"
+    } else {
+    let mainCounteryName = countrySelectBox.value
+    let mainCounteryCities = countriesData[mainCounteryName]
+
+    citySelect.innerHTML = '';
+
+    mainCounteryCities.forEach(function (city) {
+
+        citySelect.innerHTML += '<option>' + city + '</option>'
+    })
+    }
+
+
+})
