743eae17c1e2595264df108040cdb73ce2a59f39
Blacker <parsafarivash5713@gmail.com~>
2024-09-07 19:30:00 +0330

    I have completed the Modal project

--- js/script.js
+++ js/script.js
@@ -143,3 +143,38 @@ const inputMaxLength = inputElem.getAttribute('maxlength')
 inputElem.addEventListener('keyup', function () {
     spanElem.innerHTML = inputMaxLength - inputElem.value.length
 })
+
+const button = $.querySelector('p button')
+const modalParent = $.querySelector('.modal-parent')
+const x = $.querySelector('.X')
+const sectionElem = $.querySelector('section')
+
+
+function showModal () {
+    modalParent.style.display = 'block'
+    sectionElem.style.filter = 'blur(10px)'
+}
+
+function hideModalWithX () {
+    modalParent.style.display = 'none'
+    sectionElem.style.filter = 'blur(0px)'
+}
+
+function hideModalWithEsc(event) {
+    console.log(event);
+    if (event.keyCode === 13) {
+        modalParent.style.display = 'none'
+        sectionElem.style.filter = 'blur(0px)'
+
+    }
+}
+function hideModalWithEsc(event) {
+    if (event.keyCode === 32) {
+        modalParent.style.display = 'none'
+        sectionElem.style.filter = 'blur(0px)'
+    }
+}
+
+button.addEventListener('click', showModal)
+x.addEventListener('click', hideModalWithX)
+document.body.addEventListener('keyup', hideModalWithEsc)
