ae7c634cb6872be151dd150518e91554eb9be20a
Blacker <parsafarivash5713@gmail.com~>
2024-10-12 21:27:59 +0330

    completedand  refactored the Weather app  project

--- js/script3.js
+++ js/script3.js
@@ -1,112 +1,60 @@
 ﻿"use strict"
 
-let citiesData = {
-    tehran: { city: `Tehran`, temp: 12, weather: `sunny`, humidity: 23, windSpeed: 32 },
-    qom: { city: `Qom`, temp: 50, weather: `sunny`, humidity: 1, windSpeed: 0 },
-    shiraz: { city: `Shiraz`, temp: 16, weather: `rainy`, humidity: 40, windSpeed: 29 },
-    tabriz: { city: `Tabriz`, temp: 89, weather: `snowy`, humidity: 33, windSpeed: 99 },
-    nakojaAbad: { city: `NakojaAbad`, temp: 100, weather: `rainy`, humidity: 999, windSpeed: 999 },
-    mashhad: { city: `Mashhad`, temp: 1, weather: `sunny`, humidity: 6, windSpeed: 666 },
-    esfehan: { city: `Esfehan`, temp: -66, weather: `snowy`, humidity: 88, windSpeed: 1000 },
-}
-
 let $ = document
-let searchBtn = $.getElementById(`search`)
-let searchBar = $.querySelector(`.search-bar`)
 
-searchBtn.addEventListener(`click`, function() {
-    generateNewcitiesData()
-})
+const inputElem = $.querySelector(`input`)
 
+let apiData = {
+    url: `https://api.openweathermap.org/data/2.5/weather?q=`,
+    key: `f14909fd3575f37b7f6a5bc9ae78a9cf`
+}
 
-searchBar.addEventListener(`keydown`, function(event) {
+function fatchData() {
+    let countryValue = inputElem.value
+
+    fetch(`${apiData.url}${countryValue}&appid=${apiData.key}`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+
+            showData(data)
+        })
+}
+
+function showData(data) {
+    let cityElem = $.querySelector(`.city`)
+    cityElem.innerHTML = `${data.name}, ${data.sys.country}`
+
+    let dateElem = $.querySelector(`.date`)
+    dateElem.innerHTML = showDate()
+
+    let tempElem = $.querySelector(`.temp`)
+    tempElem.innerHTML = `${Math.floor(data.main.temp - 273.15)}°c`
+
+    let weatherElem = $.querySelector(`.weather`)
+    weatherElem.innerHTML = `${data.weather[0].main}`
+
+    let tempsElem = $.querySelector(`.hi-low`)
+    tempsElem.innerHTML = `${Math.floor(data.main.temp_min - 273.15)}°c / ${Math.floor(data.main.temp_max - 273.15)}°c`
+}
+
+function showDate() {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+    let now = new Date()
+
+    let day = days[now.getDay()]
+    let month = months[now.getMonth()]
+    let year = now.getFullYear()
+    let date = now.getDate()
+
+    return `${day} ${month} ${year} ${date}`
+}
+
+inputElem.addEventListener(`keypress`, (event) => {
     if (event.keyCode === 13) {
-        generateNewcitiesData()
+        fatchData()
     }
 })
-
-function generateNewcitiesData() {
-    let searchBarValue = searchBar.value.toLowerCase()
-    let mainCityDatas = citiesData[searchBarValue]
-
-    console.log(mainCityDatas)
-
-    if (mainCityDatas) {
-        $.querySelector(`.city`).innerHTML = "Weather in " + mainCityDatas.city
-        $.querySelector(`.temp`).innerHTML = mainCityDatas.temp + `°C`
-        $.querySelector(`.description`).innerHTML = mainCityDatas.weather
-        $.querySelector(`.humidity`).innerHTML = "Humidity: " + mainCityDatas.humidity
-        $.querySelector(`.wind`).innerHTML = "Wind speed: " + mainCityDatas.windSpeed + `km/h`
-        $.querySelector(`.weather`).classList.remove(`loading`)
-    } else {
-        alert(`شهر مورد نظر را به درستی وارد نمایید`)
-    }
-}
-
-// let citiesData = [
-//   {city: `tehran `, temp: 12, weather: `sunney`, humidity: 23, windSpeed: 32 },
-//   {city: `qom `, temp: 50, weather: `sunney`, humidity: 1, windSpeed: 0 },
-//   {city: `shiraz `, temp: 16, weather: `rainy`, humidity: 40, windSpeed: 29 },
-//   {city: `tabriz `, temp: 89, weather: `snowy`, humidity: 33, windSpeed: 99 },
-//   {city: `nakojaAbad `, temp: 100, weather: `rainy`, humidity: 999, windSpeed: 999 },
-//   {city: `mashhad `, temp: 1, weather: `sunney`, humidity: 6, windSpeed: 666 },
-//   {city: `esfehan `, temp: -66, weather: `snowy`, humidity: 88, windSpeed: 1000 },
-// ]
-
-// let searchBtn = $.getElementById(`search`)
-// let searchBar = $.querySelector(`.search-bar`)
-
-// searchBtn.addEventListener(`click`, function () {
-//     let searchBarValue = searchBar.value
-
-//   let mainCityDatas = citiesData.find(function (item) {
-//     return item.city === searchBarValue
-//     })
-
-//     console.log(mainCityDatas)
-
-//     if (mainCityDatas) {
-//       $.querySelector(`.city`).innerHTML = "Weather in " + mainCityDatas.city
-//       $.querySelector(`.temp`).innerHTML = mainCityDatas.temp + `°C`
-//       $.querySelector(`.description`).innerHTML = mainCityDatas.weather
-//       $.querySelector(`.humidity`).innerHTML = "Humidity: " +  mainCityDatas.humidity
-//       $.querySelector(`.wind`).innerHTML = "Wind speed: " +  mainCityDatas.windSpeed + `km/h`
-//       $.querySelector(`.weather`).classList.remove(`loading`)
-//     } else {
-//       alert(`شهر مورد نظر را به درستی وارد نمایید`)
-//     }
-
-//   })
-
-let image = $.getElementById(`B1`)
-
-const contextMenu = $.getElementById(`contextMenu`)
-
-
-function contextHandler(event) {
-    event.preventDefault()
-
-    if (contextMenu.style.display === `none`) {
-
-        contextMenu.style.left = event.pageX + `px`
-        contextMenu.style.top = event.pageY + `px`
-
-        contextMenu.style.display = `block`
-    } else {
-        contextMenu.style.left = event.pageX + `px`
-        contextMenu.style.top = event.pageY + `px`
-    }
-}
-
-function clickConTextMenu() {
-    contextMenu.style.display = `none`
-}
-
-function keyDownHandler(event) {
-    if (event.keyCode === 27)
-        contextMenu.style.display = `none`
-}
-
-$.body.addEventListener(`contextmenu`, contextHandler)
-$.body.addEventListener(`click`, clickConTextMenu)
-$.body.addEventListener(`keydown`, keyDownHandler)
