bf9780aac51121d153b40d08891d027e6c8ee4e8
Blacker <parsafarivash5713@gmail.com~>
2024-10-16 10:26:59 +0330

    completed the LocalWeatherApp project

--- js/script3.js
+++ js/script3.js
@@ -5,21 +5,54 @@ let $ = document
 const inputElem = $.querySelector(`input`)
 
 let apiData = {
-    url: `https://api.openweathermap.org/data/2.5/weather?q=`,
+    url: `https://api.openweathermap.org/data/2.5/weather?`,
     key: `f14909fd3575f37b7f6a5bc9ae78a9cf`
 }
+
+
+function getUserLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(successLocation, errorLocation)
+    } else {
+        alert("Geolocation is not supported by this browser")
+    }
+}
+
+
+function successLocation(position) {
+    const lat = position.coords.latitude
+    const lon = position.coords.longitude
+
+    fetchWeatherByLocation(lat, lon)
+}
+
+
+function errorLocation() {
+    alert("Unable to retrieve your location. Please enter a city manually")
+}
+
+
+function fetchWeatherByLocation(lat, lon) {
+    fetch(`${apiData.url}lat=${lat}&lon=${lon}&appid=${apiData.key}`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            showData(data)
+        })
+}
+
 
 function fatchData() {
     let countryValue = inputElem.value
 
-    fetch(`${apiData.url}${countryValue}&appid=${apiData.key}`)
+    fetch(`${apiData.url}q=${countryValue}&appid=${apiData.key}`)
         .then(res => res.json())
         .then(data => {
             console.log(data)
-
             showData(data)
         })
 }
+
 
 function showData(data) {
     let cityElem = $.querySelector(`.city`)
@@ -38,9 +71,9 @@ function showData(data) {
     tempsElem.innerHTML = `${Math.floor(data.main.temp_min - 273.15)}°c / ${Math.floor(data.main.temp_max - 273.15)}°c`
 }
 
+
 function showDate() {
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 
     let now = new Date()
@@ -53,8 +86,12 @@ function showDate() {
     return `${day} ${month} ${year} ${date}`
 }
 
+
 inputElem.addEventListener(`keypress`, (event) => {
     if (event.keyCode === 13) {
         fatchData()
     }
 })
+
+
+window.addEventListener("load", getUserLocation)
